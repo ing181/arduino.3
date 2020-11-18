ff567f1c26991bb64c80eebac40f52f134ff36fc
posktomten <ic_0002@ceicer.com>
2020-11-18 09:19:25 +0100

    Version 1

---
+++ 2020-11-18/första_programmet.cpp
@@ -0,0 +1,10 @@
+// Example program
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+      // En kommentar, bara för programmeraren
+      // skärmen utmatningsoperatorn "Det som matas ut" ;
+      cout << "Hej pa dig";
+}
