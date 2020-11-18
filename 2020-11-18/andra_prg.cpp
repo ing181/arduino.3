437b30359fcf30eea23841b43ebfd6aa61cd19c5
posktomten <ic_0002@ceicer.com>
2020-11-18 09:33:05 +0100

    Andra programmet

---
+++ 2020-11-18/andra_prg.cpp
@@ -0,0 +1,18 @@
+// Example program
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+      // En kommentar, bara för programmeraren
+      // skärmen utmatningsoperatorn "Det som matas ut" ;
+      // cout << "Hej pa dig";
+      
+      // variabler, där du kan förvara data
+      // datatyp namn tilldelningsoperatorn värde ;
+      int antal_ar = 56;
+      // endl; betyder slut på raden, ny rad
+      cout << "antal_ar" << endl;
+      cout << antal_ar;
+      
+}
