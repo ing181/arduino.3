6d65f40d6f587d92c2fee6ed3713dba9bf370433
posktomten <ic_0002@ceicer.com>
2020-11-20 16:13:29 +0100

    Koden från 2020-11-20

---
+++ C++/2020-11-20/Första programmet/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{   system("chcp 1252"); // Sv. tecken
+    // skärmen utmatningsoperatorn "det som skrivs ut"
+    cout << "Heja GAIS";
+    return 0;
+}
