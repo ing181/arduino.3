d756843fd92663aa0157d743d4b523a8f1590c40
posktomten <ic_0002@ceicer.com>
2020-11-20 14:25:43 +0100

    Lektion 1

---
+++ Arduino/sketch1/sketch1.ino
@@ -0,0 +1,8 @@
+void setup(){
+  // put your setup code here, to run once:
+  Serial.begin(9600); // Startar förbindelse med din Windows dator                   // "9600" är bauderate, betecknar hastighten på förbindelse
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+Serial.print("Hejsan snart tar vi rast\n");
+}
