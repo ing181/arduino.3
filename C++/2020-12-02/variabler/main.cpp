de460ff0088b8a91fd46bb82aea9c06b037ae714
posktomten <ic_0002@ceicer.com>
2020-12-02 09:01:59 +0100

    Visar eleverna

---
+++ C++/2020-12-02/variabler/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+// Handlar om in- och utmatning och variabler
+int main()
+{
+// "skärmen" "utmatningsoperatorn" "text som matas ut"
+// "utmatningsoperatorn" "nyrad" "text som matas ut"
+// "nyrad"
+    cout << "Hello world!" << endl;
+    cout << "HEJ" << endl;
+    return 0;
+}
