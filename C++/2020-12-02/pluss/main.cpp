cfcb7fb71e866728ac967a155f8b2b85e8eaf159
posktomten <ic_0002@ceicer.com>
2020-12-02 09:56:16 +0100

    Visar eleverna 2

---
+++ C++/2020-12-02/pluss/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+// Uppgift
+// Nu behöver du två int variabler
+// Låt användaren mata in ett heltal
+// i respektive variabel.
+// Låt programmet beräkna och skriva
+// ut summan av de två talen
+// Ledtråd: + fungerar precis som ni
+// är vana vid
+int main()
+{
+
+    return 0;
+}
--- C++/2020-12-02/variabler/main.cpp
+++ C++/2020-12-02/variabler/main.cpp
@@ -3,10 +3,27 @@ using namespace std;
 // Handlar om in- och utmatning och variabler
 int main()
 {
+ //  system("chcp 1252");
 // "skärmen" "utmatningsoperatorn" "text som matas ut"
 // "utmatningsoperatorn" "nyrad" "text som matas ut"
 // "nyrad"
     cout << "Hello world!" << endl;
     cout << "HEJ" << endl;
+// "tangentbordet" "inmatningsoperatorn"
+//    cin >> Vi behöver en behållare
+//    där vi placerar det vi matat in
+// Om behållare, variabler
+// Om datatyper
+// behållare för heltal kallas int
+// int, från integer
+// Nu skapar jag en varibel för heltal
+    int alder; // Varibeln innehåller
+    // ett odifinierat värde
+    cout << alder << endl;
+    // Användaren uppmanas att ange sin ålder
+    cout << "Mata in din ålder" << endl;
+    // "tangentbordet" "inmatningsoperatorn"
+    cin >> alder;
+    cout << "Din ålder är " << alder << endl;
     return 0;
 }
