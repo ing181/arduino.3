9ff35fc30083988ae7a031409e8bdcbf1f75893a
ing181 <ingemar.ceicer@lerum.se>
2020-12-16 09:50:56 +0100

    kommentarer

--- C++/2020-12-16/kort om variabler och if while/main.cpp
+++ C++/2020-12-16/kort om variabler och if while/main.cpp
@@ -12,20 +12,21 @@ int main()
     float flyttal;  // Normalt i vår miljö (Windows 10) används 8 decimalers noggranhet.
 
     // utmatning
-   // cout << "Mata in ett flyttal ";
-   // cin >> flyttal;
+     cout << "Mata in ett flyttal ";
+     // inmatning
+     cin >> flyttal;
 
     // Enkel villkorssats
     // OBS! Jämförelse mellan två saker sker med
-    // jämförelseoperatorn "==" TVÅ LIKAMEDS TECKEN
-    /*
-    if (flyttal == 7.000001) {
+    // jämförelseoperatorn "==" TVÅ LIKAMED TECKEN
+
+    if (flyttal == 7.000001) {  // Flyttal är aldrig exakta. Datorn kan ha svårt att avgöra om detta är sant.
         cout << "SANT" << endl;
     }
     else {
         cout << "FALSKT" << endl;
     }
-*/
+
     /*
     Det som står i villkoret för if-satsen
     måste gå att entydigt bestämma om det
@@ -43,11 +44,11 @@ int main()
         cout << "Nummer 1" << endl;
         cout << "Nummer 2" << endl;
         cout << "Nummer 3" << endl;
-        // Vi behöver få till en ändrin av
-        // OBS! Tilldelning "=". "Det som är till vänster får samma värde som det som är till höger"
+        // Vi behöver få till en ändrin  för att få slut på loopen.
+        // OBS! Tilldelning "=". "Det som är till vänster får värdet av det som är till höger"
         heltal = (heltal + 1); // 10
         cout << "heltal har nu värdet " << heltal << endl;
-        if (heltal > 9) {
+        if (heltal > 9) {  // Här ändras värdet på heltal, loopen kommer aldrig att ta slut
             heltal = 0;
         }
     }
